1fcb22df9fcea21ee74ad2144bf03e2680c51222
Zyxxxxxi <1069227225@qq.com>
2022-11-13 00:31:03 +0800

    init(project):初始化前后端项目

---
+++ server/src/main/java/com/laquanquan/personnelsalary/PersonnelSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.laquanquan.personnelsalary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelSalaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelSalaryApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/laquanquan/personnelsalary/PersonnelSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laquanquan.personnelsalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelSalaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
